1768fa7974da9aed4a5601293aeac8ed43e6cb6b
Lopsan Antonio Molina Osorio <lmolinao1200@alumno.ipn.mx>
2020-05-10 03:58:46 -0500

    adding status choices

--- pod/apps/accounts/models.py
+++ pod/apps/accounts/models.py
@@ -1,17 +1,29 @@
 import uuid
+from enum import Enum
 from django.db import models
 from pod.apps.groups.models import Group
 
 
 class Account(models.Model):
+    class STATUS(Enum):
+        ini = ("DES", "DESEMBOLSADA")
+        fin = ("CER", "CERRADA")
+
+    @classmethod
+    def get_value(cls, val):
+        try:
+            return cls[val].value[1]
+        except KeyError:
+            return False
+
     class Meta:
         db_table = 'Cuentas'
 
     id = models.CharField(primary_key=True, default=str(uuid.uuid4())[-6:-1].upper(), editable=False, max_length=5)
     group = models.ForeignKey(Group, on_delete=models.CASCADE, db_column='grupo_id')
-    status = models.CharField(max_length=50, db_column='status')
-    amount = models.CharField(max_length=50, db_column='monto')
-    balance = models.CharField(max_length=50, db_column='saldo')
+    status = models.CharField(max_length=50, db_column='status', choices=[x.value for x in STATUS])
+    amount = models.DecimalField(max_digits=15, decimal_places=2, db_column='monto')
+    balance = models.DecimalField(max_digits=15, decimal_places=2, db_column='saldo')
 
     @classmethod
     def create(cls, *args):
--- pod/apps/payments/models.py
+++ pod/apps/payments/models.py
@@ -1,19 +1,33 @@
 import uuid
 import datetime
+from enum import Enum
 from django.db import models
 from pod.apps.accounts.models import Account
 
 
 class PaymentCalendar(models.Model):
+    class STATUS(Enum):
+        pen = ('PEN', 'PENDIENTE')
+        pag = ('PAG', 'PAGADO')
+        par = ('PAR', 'PARCIAL')
+        atr = ('ATR', 'ATRASADO')
+
+        @classmethod
+        def get_value(cls, val):
+            try:
+                return cls[val].value[1]
+            except KeyError:
+                return False
+
     class Meta:
         db_table = 'CalendarioPagos'
 
     id = models.IntegerField(primary_key=True, default=int(str(uuid.uuid4().fields[-1])[:11]), editable=False)
     account = models.ForeignKey(Account, on_delete=models.CASCADE, db_column='cuenta_id')
-    num_payment = models.CharField(max_length=50, db_column='num_pago')
-    amount = models.CharField(max_length=50, db_column='monto')
+    num_payment = models.IntegerField(db_column='num_pago')
+    amount = models.DecimalField(max_digits=15, decimal_places=2, db_column='monto')
     payment_date = models.DateField(db_column='fecha_pago')
-    status = models.CharField(max_length=50, db_column='estatus')
+    status = models.CharField(max_length=15, db_column='estatus', choices=[x.value for x in STATUS])
 
     @classmethod
     def create(cls, *args):
